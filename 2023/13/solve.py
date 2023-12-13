7745b0b36b80e03320e21a75bc13a143ba903f89
MigFerro <migueloliveiraferro@gmail.com>
2023-12-13 06:18:19 +0000

    2023 day 13 improvements

--- 2023/13/solve.py
+++ 2023/13/solve.py
@@ -8,7 +8,7 @@ def read_input():
     else: 
         return open('input.txt', 'r').read().strip()
 
-def sim_ind(G):
+def sim_ind(G, part2=False):
     R = len(G)
     for i in range(len(G)-1):
         ra = i+1
@@ -16,25 +16,31 @@ def sim_ind(G):
         if ra < rb:
             above = G[0:ra]
             bellow = G[ra:ra+ra]
-        if rb < ra:
+        elif rb < ra:
             above = G[R-2*rb:R-rb]
             bellow = G[R-rb:R]
-        if ra == rb:
+        elif ra == rb:
             above = G[0:ra]
             bellow = G[ra:]
+
         sa = ''.join([''.join(row) for row in above])
         sb = ''.join([''.join(row) for row in bellow[::-1]])
         
-        diff = sum(sa[i] != sb[i] for i in range(len(sa)))
+        if part2:
+            diff = sum(sa[i] != sb[i] for i in range(len(sa)))
 
-        if diff == 1:
-            return ra
+            if diff == 1:
+                return ra
+
+        if not part2:
+            if sa == sb:
+                return ra
 
     return None
 
 
 
-def solve():
+def solve(part2=False):
     f = read_input()
     s = f.split("\n\n")
 
@@ -44,25 +50,24 @@ def solve():
         G = [list(line) for line in pattern.split("\n")]
         found = False
         
-        ra = sim_ind(G)
+        ra = sim_ind(G, part2)
         if ra is not None:
             found = True
             ans += 100*ra
 
         if not found:
-            T = list(map(list, zip(*G)))
-            rl = sim_ind(T)
+            #transpose G
+            T = list(map(list, zip(*G))) 
+            rl = sim_ind(T, part2)
             if rl is not None:
                 found = True
                 ans += rl
 
+        #sanity check
         assert found
     print(ans)
             
 
-def solve_2():
-    print("\n")
-
 if __name__ == "__main__":
     solve()
-    # solve_2()
+    solve(part2=True)
